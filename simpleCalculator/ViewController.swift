f66d167c137938e68e8192a2614225dd21a0bd7f
Colin Wallace <68976883+cjw0711@users.noreply.github.com>
2023-01-29 23:12:53 -0600

    Add files via upload

---
+++ simpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 2
+//
+//  Created by Colin Wallace on 01/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
